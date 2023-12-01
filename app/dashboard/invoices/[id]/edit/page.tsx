426a53f6acbc7f7892651d2255659f297a34e397
Juan Pablo Addeo <jpaddeo@gmail.com>
2023-11-30 22:30:43 -0300

    chore: :zap: adding changes for chapters 1 to 14

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,43 @@
+import { notFound } from 'next/navigation';
+
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+
+type DashboardInvoicesEditPageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function DashboardInvoicesEditPage({
+  params,
+}: DashboardInvoicesEditPageProps) {
+  const { id } = params;
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  if (!invoice) {
+    notFound();
+  }
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
